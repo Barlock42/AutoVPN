66565021a5e28e0d0f0abc5d217d7b2ebe7d0e47
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-03-17 19:10:48 +0900

    xlsx

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const cors = require('cors');
+const XLSX = require('xlsx');
 
 const app = express();
 app.use(cors({
@@ -11,8 +12,15 @@ app.use(cors({
 
 app.post('/api/user', (req, res) => {
   const { name, surname, email } = req.body;
-  // process the form data here
-  // ...
+  // Load the Excel file
+  const workbook = XLSX.readFile('/Users/konstantin/Documents/GitHub/AutoVPN/src/data/User_Download_28022023_154740.xlsx');
+  // Get the first sheet name;
+  const sheetName = workbook.SheetNames[0];
+  // Get the sheet
+  const sheet = workbook.Sheets[sheetName];
+  // Convert the sheet to JSON object
+  console.log(sheet);
+  //const data = XLSX.utils.sheet_to_json(sheet);
   // send the response back to the client
   res.json({ message: 'Form submitted successfully!' });
 });
