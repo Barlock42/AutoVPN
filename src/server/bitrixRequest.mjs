75d80d9f2820e664243be5eb94de92b6abda5f60
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-04-04 23:26:42 +0900

    request

--- src/server/bitrixRequest.mjs
+++ src/server/bitrixRequest.mjs
@@ -1,21 +1,22 @@
 import axios from "axios";
-import { config } from './config.mjs';
+import { config } from "./config.mjs";
 
 export function bitrixRequest() {
-  axios
-    .get( config.bitixAddress , {
+  return axios
+    .get(config.bitixAddress, {
       params: {
-        filter: {},
-        auth: "your-auth-token",
+        filter: { ACTIVE: "1" },
         // add any other parameters required by the API method
       },
     })
     .then((response) => {
       // handle successful response
       console.log(response.data.result);
+      return response.data.result;
     })
     .catch((error) => {
       // handle error
       console.error(error);
+      throw error;
     });
 }
--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,20 +1,20 @@
 import { generateRandomCode } from "./codeGen.mjs";
-import { config } from './config.mjs';
+import { bitrixRequest } from "./bitrixRequest.mjs";
+import { config } from "./config.mjs";
 
 // Node.js server that handles form submission
 import express from "express";
 import bodyParser from "body-parser";
 
 import cors from "cors";
-import XLSX from "xlsx";
 
 import AWS from "aws-sdk";
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // disables the SSL/TLS certificate verification for all HTTPS requests
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // disables the SSL/TLS certificate verification for all HTTPS requests
 const ses = new AWS.SES({
   accessKeyId: config.accessKeyId,
   secretAccessKey: config.secretAccessKey,
-  region: config.region // replace with your preferred region
+  region: config.region, // replace with your preferred region
 });
 
 const sendVerificationEmail = async (email, verificationLink) => {
@@ -25,14 +25,14 @@ const sendVerificationEmail = async (email, verificationLink) => {
     Message: {
       Body: {
         Html: {
-          Charset: 'UTF-8',
+          Charset: "UTF-8",
           Data: `Введите 6-значный код ${userCode} для подтверждения.
           Click <a href="${verificationLink}">here</a> to verify your email address.`,
         },
       },
       Subject: {
-        Charset: 'UTF-8',
-        Data: 'Verify your email address',
+        Charset: "UTF-8",
+        Data: "Verify your email address",
       },
     },
     Source: config.email, // replace with your verified email address
@@ -42,7 +42,9 @@ const sendVerificationEmail = async (email, verificationLink) => {
     await ses.sendEmail(params).promise();
     console.log(`Verification email sent to ${email}`);
   } catch (err) {
-    console.error(`Error sending verification email to ${email} from ${config.email}: ${err.message}`);
+    console.error(
+      `Error sending verification email to ${email} from ${config.email}: ${err.message}`
+    );
   }
 };
 
@@ -57,51 +59,35 @@ app.use(
 
 app.post("/api/user", (req, res) => {
   const { name, surname, email } = req.body;
-  // Load the Excel file
-  const workbook = XLSX.readFile(
-    "/Users/konstantin/Documents/GitHub/AutoVPN/src/data/User_Download_28022023_154740.xlsx"
-  );
-  // Get the first sheet name;
-  const sheetName = workbook.SheetNames[0];
-  // Get the sheet
-  const sheet = workbook.Sheets[sheetName];
-  // Convert the sheet to JSON object
-  //const data = XLSX.utils.sheet_to_json(sheet);
 
-  // Loop through all rows in the sheet and check if the data exists
-  for (let i = 2; ; i++) {
-    // Assuming row 1 is header
-    const nameCell = sheet[`A${i}`]; // Assuming name column is A
-    if (!nameCell) {
-      // End loop if cell is empty
-      break;
-    }
-    const curName = nameCell.v; // Get the name value
+  bitrixRequest()
+    .then((json) => {
+      // use the JSON data returned by the Bitrix API
+      const emails = json.map((item) => item.EMAIL);
+      //console.log(emails);
+      
+      // Loop through all emails
+      for (let i = 1; ; i++) {
+        if (emails[i] === email) {
+          // Check if all data matches
+          console.log(
+            `${name} ${surname} with email ${email} exists and active in the Excel file.`
+          );
+          console.log(userCode);
+          sendVerificationEmail(
+            email,
+            "http://localhost:4000/api/verification/"
+          );
+          break; // Exit loop if data is found
+        }
+      }
+      
+    })
+    .catch((error) => {
+      // handle error
+      console.error(error);
+    });
 
-    const surnameCell = sheet[`B${i}`]; // Assuming surname column is B
-    const curSurname = surnameCell ? surnameCell.v : ""; // Get the surname value or empty string
-
-    const emailCell = sheet[`C${i}`]; // Assuming email column is C
-    const curEmail = emailCell ? emailCell.v : ""; // Get the email value or empty string
-
-    const statusCell = sheet[`D${i}`]; // Assuming status column is D
-    const curStatus = statusCell ? statusCell.v : ""; // Get the status value or empty string
-
-    if (
-      curName === name &&
-      curSurname === surname &&
-      curEmail === email &&
-      curStatus === "Active"
-    ) {
-      // Check if all data matches
-      console.log(
-        `${name} ${surname} with email ${email} exists and active in the Excel file.`
-      );
-      console.log(userCode);
-      sendVerificationEmail(email, "http://localhost:4000/api/verification/");
-      break; // Exit loop if data is found
-    }
-  }
   // send the response back to the client
   res.json({ message: "Form submitted successfully!" });
 });
