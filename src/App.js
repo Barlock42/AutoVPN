5061165497bf0367870eac164004e694867a965f
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-04-10 00:55:20 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import SignupForm from "./pages/SignupForm.tsx";
 import SignResultForm from "./pages/SignResultForm.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="/" element={<SignupForm />} />
             <Route path="/result" element={<SignResultForm />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </header>
     </div>
   );
