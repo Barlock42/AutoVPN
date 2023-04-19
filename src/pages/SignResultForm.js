a4d9a26a9642579ce447619abe27712d17636aa6
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-04-19 20:55:43 +0900

    ssh2

--- src/pages/SignResultForm.js
+++ src/pages/SignResultForm.js
@@ -6,27 +6,9 @@ function SignResultForm() {
   const searchParams = new URLSearchParams(window.location.search);
   const variable = JSON.parse(searchParams.get("variable"));
   // console.log(variable);
-
-  const handleDownloadClick = () => {
-    fetch("http://localhost:4000/api/verification/download")
-      .then((res) => {
-        return res.blob();
-      })
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", "test.txt");
-        document.body.appendChild(link);
-        link.click();
-      });
-  };
-
   return (
     <div>
       <h2>{variable}</h2>
-      {/* <Link onClick={handleDownloadClick}>Загрузить сертификат</Link> */}
-      {/* <br/> */}
       <Link to="/">Вернуться на предыдущую страницу</Link>
     </div>
   );
--- src/server/sshBash.mjs
+++ src/server/sshBash.mjs
@@ -1,17 +1,26 @@
-import { spawn } from 'child_process';
+import { spawn } from "child_process";
 // import project config
 import { config } from "./config.mjs";
+// import sftp client
+import { Client } from 'ssh2';
+import fs from 'fs';
+
+// Replace the values with your server and key details
+const server = {
+  host: config.certifcateAuthority.IPAddress,
+  username: config.certifcateAuthority.UserName,
+  privateKey: config.certifcateAuthority.pathToRSAKey,
+  pathToScript: config.certifcateAuthority.pathToScript,
+};
+
+const sshConfig = {
+  host: server.host,
+  port: '22',
+  username: server.username,
+  privateKey: server.privateKey
+};
 
 export function runScript() {
-
-  // Replace the values with your server and key details
-  const server = {
-    host: config.certifcateAuthority.IPAddress,
-    username: config.certifcateAuthority.UserName,
-    privateKey: config.certifcateAuthority.pathToRSAKey,
-    pathToScript: config.certifcateAuthority.pathToScript,
-  };
-
   // Replace the path and filename with your Bash script details
   const script = spawn("ssh", [
     "-i",
@@ -30,6 +39,27 @@ export function runScript() {
   });
 
   script.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
+    // console.log(`child process exited with code ${code}`);
   });
+}
+
+export function getCert() {
+  console.log('getCert')
+  const conn = new Client();
+  conn.on('ready', function() {
+    console.log('Client :: ready');
+    conn.sftp(function(err, sftp) {
+      if (err) throw err;
+      const remoteFilePath = config.certifcateAuthority.filePath + config.certifcateAuthority.fileName;
+      const localFilePath = config.server.certsPath + config.certifcateAuthority.fileName;
+      const readStream = sftp.createReadStream(remoteFilePath);
+      const writeStream = fs.createWriteStream(localFilePath);
+      readStream.pipe(writeStream);
+      readStream.on('close', function() {
+        console.log('File transfer completed');
+        conn.end();
+      });
+    });
+  }).connect(sshConfig);
+
 }
